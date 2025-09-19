27dab411814c33b242f2baca0dfd11a4628e5073
Teja-Coding <dropteja@gmail.com>
2025-09-19 15:23:57 +0530

    just add the varibale

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "Ramesh:: This is Variables file"
-echo "suresh:: This is Variables file"
-echo "Ramesh:: This is Variables file"
-echo "suresh:: This is Variables file"
+
+#now i need to store the value in variable 
+name1="Ramesh"
+name2="suresh"
+
+echo "$name1:: This is Variables file"
+echo "$name2:: This is Variables file"
+echo "$name1:: This is Variables file"
+echo "$name2:: This is Variables file"
