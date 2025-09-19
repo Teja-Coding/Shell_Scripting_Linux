fadfb0bcbc802fdf0f00e5a14fad6cfe5a54526b
Teja-Coding <dropteja@gmail.com>
2025-09-19 15:32:51 +0530

    just changes

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,11 @@
 name1="Ramesh"
 name2="suresh"
 
+myName="Ramesh Suresh"
+myFullName="Ramesh Suresh Kumar"
+
+echo "My name is: $myName"
+echo "My full name is: $myFullName"
 echo "$name1:: This is Variables file"
 echo "$name2:: This is Variables file"
 echo "$name1:: This is Variables file"
